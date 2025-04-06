1bb05b542289439bbcb8fe4559f9239b8b74c7ca
Mickael Joseph <mickaelandriana06@gmail.com>
2025-04-06 23:11:22 +0300

    create module for send and receiving img--create the function for sending img

--- src/scripts/screen.py
+++ src/scripts/screen.py
@@ -1,9 +1,22 @@
 import numpy as np 
-import cv2 as cv
+import pickle as pkl
 from mss import mss
 
 def send_screen(conn):
     """this function is used to send screen from the server to the client"""
     with mss() as sct :
         monitor = {'top':0, 'left':0, 'widht':800, 'height':600}
+        
+        while 'running':
+            try:
+                 img = np.array(sct.grab(monitor))
+
+                 """encode data with pickle"""
+                 img = pkl.dumps(img)
+                 conn.send(img)
+            except KeyboardInterrupt:
+                print('finishing transfer.....')
+                break
+    
+
 
