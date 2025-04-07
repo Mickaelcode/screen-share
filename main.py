93556fc61b72b933f788c9722738b4e8314706db
Mickael Joseph <mickaelandriana06@gmail.com>
2025-04-07 14:40:01 +0300

    add a  check module

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import argparse
 import os 
+import pkgutil 
+
 
 parser = argparse.ArgumentParser(description='show who is the server and client')
 parser.add_argument('-U','--user', help='c if client or s for server')
 parser.add_argument('-H', '--host', help='host machine')
 parser.add_argument('-P', '--port',type=int,  help='Port for listen')
 args = parser.parse_args()
+
+
+module_name = ['numpy','mss','opencv-python']
+for module in module_name:
+    loader = pkgutil.find_loader(module)
+    if not loader :
+        os.system(f'pip install {module}')
+
 
 
 if not args.host or not args.port :
