dbe5e0baeac598fdc14e3f5f753713d8c05e3e58
Mickael Joseph <mickaelandriana06@gmail.com>
2025-04-07 08:21:59 +0300

    change a key word

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ args = parser.parse_args()
 if not args.host or not args.port :
     print('host and port required')
 
-elif not args.user or args.user is 's':
+elif not args.user or args.user == 's':
     os.system(f'python3 src/scripts/server.py {args.host} {args.port}')
 else:
     os.system((f'python3 src/scripts/client.py {args.host} {args.port}'))
