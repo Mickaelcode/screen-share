32c011877ccebbb01f1180d5ff858e1a03aafe52
Mickael Joseph <mickaelandriana06@gmail.com>
2025-04-07 18:59:22 +0300

    add check plateform before executing script

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import os 
 import pkgutil 
+import platform
 
 
 parser = argparse.ArgumentParser(description='show who is the server and client')
@@ -17,13 +18,21 @@ for module in module_name:
         os.system(f'pip install {module}')
 
 
+"""check the plateform before executing the scripts"""
+
+executer = ''
+if platform.system()=='Linux':
+    executer = 'python3'
+else:
+    executer = 'python'
+
 
 if not args.host or not args.port :
     print('host and port required')
 
 elif not args.user or args.user == 's':
-    os.system(f'python3 src/scripts/server.py {args.host} {args.port}')
+    os.system(f'{executer} src/scripts/server.py {args.host} {args.port}')
 else:
-    os.system((f'python3 src/scripts/client.py {args.host} {args.port}'))
+    os.system((f'{executer} src/scripts/client.py {args.host} {args.port}'))
     
     
